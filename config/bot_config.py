6f66faa887fa8f047441bc9c300f59ac88fdae90
EmilyCV <emily_cvasconcelos@hotmail.com>
2023-06-21 20:23:46 -0300

    configuration files for bot settings

---
+++ config/bot_config.py
@@ -0,0 +1,8 @@
+from discord.ext import commands
+import discord
+
+def create_bot():
+    intents = discord.Intents.default()
+    intents.guilds = True
+    bot = commands.Bot("!", intents=intents)
+    return bot
