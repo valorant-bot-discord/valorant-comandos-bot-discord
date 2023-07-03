5387c1c4b232b459f8007ed1ca6f35f99bb5dc2b
EmanuelGCosta <emanuelgs3.abec@gmail.com>
2023-07-02 21:08:18 -0300

    adjustment on the command "!valorant"

--- bot.py
+++ bot.py
@@ -8,7 +8,8 @@ import random
 
 config = BotConfig()
 bot = create_bot()
-agent = agents()
+agents = agents()
+
 
 
 @bot.event
@@ -27,14 +28,14 @@ async def on_message(message):
 
 @bot.command(name="valorant")
 async def valorant(ctx):
-    await ctx.send(f"Por favor, digite os nomes dos esquisitos separados por vírgula.")
+    await ctx.send(f"Por favor, digite os nomes dos jogadores separados por vírgula.")
 
     def check(m):
         return m.author == ctx.author and m.channel == ctx.channel
 
     try:
         nomes_msg = await bot.wait_for("message", check=check, timeout=60)
-        if nomes_msg == "!" or nomes_msg == "!valorant":
+        if nomes_msg.content == "!" or nomes_msg.content == "!valorant":
             await ctx.send("Nome inválido.")
             return
 
@@ -44,14 +45,21 @@ async def valorant(ctx):
             await ctx.send(mensagem)
             return
 
-        for nome in nomes:
-            agent_name = random.choice(agent)
-            agent.remove(agent_name)
-            mensagem = f"{nome.title()}: {agent_name}"
-            await ctx.send(mensagem)
+        agents_aux = []
+        if len(nomes) <= len(agents):
+            for nome in nomes:
+                agent_name = random.choice(agents)
+                while agent_name in agents_aux:
+                    agent_name = random.choice(agents)
+
+                agents_aux.append(agent_name)
+                await ctx.send(f"{nome.title()}: {agent_name}")
+        else:
+            await ctx.send(f"MM Bot suporta até {len(agents)} pessoas")
 
     except asyncio.TimeoutError:
         await ctx.send("Tempo limite excedido. Por favor, tente novamente.")
+
 
 
 bot.run(config.TOKEN_BOT)
